b82d25a82d724627afcb5c6b58d77bd923f4bb3a
DylanQin4 <dylanqin4@gmail.com>
2022-12-22 18:46:02 +0100

    upload

---
+++ Client.java
@@ -0,0 +1,12 @@
+package client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import DHCP.*;
+
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Server.java
@@ -0,0 +1,15 @@
+package server;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import DHCP.*;
+
+public class Server {
+    public static void main(String[] args) {
+        
+    }
+}
